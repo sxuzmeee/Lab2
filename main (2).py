3aa180a699c49a8fb0bf781515b6767d17410713
sxuzmeee <144207608+sxuzmeee@users.noreply.github.com>
2023-10-16 11:19:39 +0300

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,45 @@
+#1
+# ref_list = [ ['Александр', 'Анастасия'], ['Смирнов',   'Иванова'] ]
+# def list_reorder(ref_list):
+#     ref_list_n = ['Александр', 'Анастасия'] # Список имен
+#     ref_list_s = ['Смирнов', 'Иванова'] # Список фамилий
+#
+#     return ref_list_n[0] +',', ref_list_s[0] + ',', ref_list_n[1] + ',', ref_list_s[1] #Вытаскивание из списков имен и фамилий
+# print(list_reorder(ref_list)) #Вывод функции
+#2
+#def del_rep
+#num = [1, 2, 1, 2, 3, 1, 2, 3, 4]
+
+#4
+#temper = ['Холодно', 'Прохладно', 'Зябко', 'Тепло', 'Жарко'] #Список температур
+# def temp_cat(temper):
+#     temp = int(input())
+#
+#     if temp < -20:
+#         print('0')
+#     elif temp == -20 or temp < 0:
+#         print('1')
+#     elif temp == 0 or temp < 15:
+#         print('2')
+#     elif temp == 15 or temp < 25:
+#         print('3')
+#     else:
+#         print('4') #Проверка условий и выдача результата
+#
+# print(temp_cat(temper)) #Вызов функции
+
+#5
+#3
+# nums = (10, 20, 30, 40, 50, 60, 70, 80, 100)
+# limit = 50
+# def lim_max(nums):
+#     if nums > 50:
+#         return limit
+#     else:
+#         print(nums)
+# print(lim_max(nums(limit())))
+
+
+
+
+
